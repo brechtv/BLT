c08ed19a91a5e38a47152c992bfc3485a6425b48
Brecht <brechtvermeire@gmail.com>
2018-11-16 12:45:38 +0000

    comments

--- ext.js
+++ ext.js
@@ -1,54 +1,51 @@
 "use strict"
 
+// ui elements
 var $counter = document.getElementById("counter")
 var $comments = document.getElementById("comments")
-var $asker = document.getElementById("asker")
+var $task = document.getElementById("task")
 var $webhook_destination = document.getElementById("webhook_destination")
 
-document.getElementById("inc1").addEventListener("click", inc1)
-document.getElementById("inc2").addEventListener("click", inc2)
-document.getElementById("inc5").addEventListener("click", inc5)
-document.getElementById("inc10").addEventListener("click", inc10)
+// button click listeners
+// increment counter by n
+document.getElementById("inc1").addEventListener("click", function(){ inc(1) })
+document.getElementById("inc2").addEventListener("click", function(){ inc(2) })
+document.getElementById("inc5").addEventListener("click", function(){ inc(5) })
+document.getElementById("inc10").addEventListener("click", function(){ inc(10) })
 
-
+// decrease counter
 document.getElementById("dec").addEventListener("click", dec)
+
+// download tracking data
 document.getElementById("dl").addEventListener("click", dl)
+
+// clear tracking data
 document.getElementById("clear").addEventListener("click", clear)
+
+// save comments
 document.getElementById("savecomments").addEventListener("click", save_comments)
+
+// send tracking data to webhook
 document.getElementById("ul").addEventListener("click", sendHook)
+
+// to set webhook destination
 document.getElementById("menu").addEventListener("click", toggleMenu)
+
+// to save the webhook destination
 document.getElementById("save_zap").addEventListener("click", save_webhook_destination)
 
+// on load, render
 window.addEventListener("load", render)
 
-function inc1() {
+// increment counter by n
+function inc(n) {
     chrome.storage.sync.get("counter", function(data) {
-        ("counter" in data ? set(data["counter"] + 1) : set(1))
+        ("counter" in data ? set(data["counter"] + n) : set(1))
     })
     incdone()
 }
 
-function inc2() {
-    chrome.storage.sync.get("counter", function(data) {
-        ("counter" in data ? set(data["counter"] + 2) : set(1))
-    })
-    incdone()
-}
-
-function inc5() {
-    chrome.storage.sync.get("counter", function(data) {
-        ("counter" in data ? set(data["counter"] + 5) : set(1))
-    })
-    incdone()
-}
-
-function inc10() {
-    chrome.storage.sync.get("counter", function(data) {
-        ("counter" in data ? set(data["counter"] + 10) : set(1))
-    })
-    incdone()
-}
-
+// when increment is done
 function incdone() {
     var sc_arr = ["Another one!", "There we go!", "Hurray!", "One more!", "Yee-haw", "Bring em on!",
         "Crazy talk!", "MOAR", "Hold the phone!", "Da-yum!", "Ludicrous", "Are you crazy?"
@@ -57,18 +54,14 @@ function incdone() {
     showAlert(msg, "alert-smurf")
 }
 
+// decrease counter
 function dec() {
     chrome.storage.sync.get("counter", function(data) {
         ("counter" in data ? set(data["counter"] - 1) : set(1))
     })
 }
 
-function add(value) {
-    chrome.storage.sync.get("counter", function(data) {
-        ("counter" in data ? set(data["counter"] + value) : set(1))
-    })
-}
-
+// clear current counter
 function clear() {
     try {
         set(0)
@@ -79,6 +72,7 @@ function clear() {
     }
 }
 
+// set the counter with a value, used with inc()
 function set(e) {
     e < 0 ? e = 0 : 999 < e && (e = 999), chrome.storage.sync.set({
         "counter": e
@@ -87,6 +81,7 @@ function set(e) {
     })
 }
 
+// render app ui with saved data from chrome storage if available
 function render() {
     chrome.storage.sync.get("counter", function(e) {
         $counter.innerText = ("counter" in e ? e.counter : 0)
@@ -107,30 +102,32 @@ function render() {
         }
     })
 
+    // commments for task, if available
     chrome.storage.sync.get("comments", function(e) {
         $comments.value = ("comments" in e ? e.comments : "")
     })
-
-    chrome.storage.sync.get("asker", function(e) {
-        $asker.value = ("asker" in e ? e.asker : "")
+    // task name, if available
+    chrome.storage.sync.get("task", function(e) {
+        $task.value = ("task" in e ? e.task : "")
     })
-
+    // get the webhook destination if available
     chrome.storage.sync.get("webhook_destination", function(e) {
         $webhook_destination.value = ("webhook_destination" in e ? e.webhook_destination : "")
     })
-
+    // can't send to webhook if there isn't a destination
     if ($webhook_destination === "") {
         document.getElementById("ul").disabled = true
     }
 }
 
+// save task, saves to storage only
 function save_comments() {
     try {
         chrome.storage.sync.set({
             "comments": $comments.value
         })
         chrome.storage.sync.set({
-            "asker": $asker.value
+            "task": $task.value
         })
         showAlert("[SUCCESS] Saved comments", "alert-smurf")
     } catch (err) {
@@ -138,15 +135,17 @@ function save_comments() {
     }
 }
 
+// clears task
 function clear_comments() {
     chrome.storage.sync.set({
         "comments": ""
     })
     chrome.storage.sync.set({
-        "asker": ""
+        "task": ""
     })
 }
 
+// save the webhook destination
 function save_webhook_destination() {
     try {
         chrome.storage.sync.set({
@@ -158,23 +157,25 @@ function save_webhook_destination() {
     }
 }
 
+// create the content for download (csv or json)
 function content_for_dl(format) {
     var content
     var today = getDateFormatted()
     if (format == "csv") {
-        content = `date,count,asker,comments\n`
-        content += today + "," + $counter.innerText + "," + $asker.value + "," + $comments.value
+        content = `date,count,task,comments\n`
+        content += today + "," + $counter.innerText + "," + $task.value + "," + $comments.value
     } else if (format == "json") {
         content = JSON.stringify({
             date: today,
             count: $counter.innerText,
-            asker: $asker.value,
+            task: $task.value,
             comments: $comments.value
         })
     }
     return content
 }
 
+// download the data
 function dl() {
     try {
         var content = content_for_dl("csv")
@@ -191,7 +192,7 @@ function dl() {
     }
 }
 
-
+// if you want to send it to a webhook
 function sendHook() {
     save_comments()
     var content = content_for_dl("json")
@@ -218,6 +219,7 @@ function sendHook() {
     xhr.send(content)
 }
 
+// helper functions
 function toggleMenu() {
     var menu = document.getElementById("menu-hidden")
     if (menu.style.display === "none") {
